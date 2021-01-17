d13a14e51411423293f4e1864c74b10f5132b35d
Benjamin Midtvedt <bmidtvedt@msn.com>
2021-01-17 05:17:59 +0100

    aligned entry page

--- src/OpenProjectPage/OpenProjectPage.tsx
+++ src/OpenProjectPage/OpenProjectPage.tsx
@@ -21,17 +21,19 @@ function OpenProjectPage(props: typeof mapDispatch) {
 
     return (
         <header className="h-screen-title bg-gradient-to-tr from-gray-900 to-gray-800 bg-blue-500 flex flex-col items-center justify-center">
-            <img className="w-56 h-56 inline-block " src={logo}></img>
-            <div className="flex place-items-center">
+            <img className="w-72 h-72 inline-block " src={logo}></img>
+            <div className="flex h-72 place-items-center">
                 <h1 className=" text-gray-300 p-10 select-none text-title">DEEPTRACK</h1>
             </div>
-
-            <RoundedButton className="w-96 mb-6 text-bread" onClick={props.openCreateProjectModal}>
-                Create a new project!
-            </RoundedButton>
-            <RoundedButton className="w-96 text-bread" onClick={openExistingProject}>
-                Load an existing project
-            </RoundedButton>
+            <div className="h-40"></div>
+            <div className="justify-self-end h-32">
+                <RoundedButton className="w-96 mr-10 text-bread" onClick={props.openCreateProjectModal}>
+                    Create a new project!
+                </RoundedButton>
+                <RoundedButton className="w-96 ml-10 text-bread" onClick={openExistingProject}>
+                    Load an existing project
+                </RoundedButton>
+            </div>
         </header>
     );
 }
