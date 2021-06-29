86b171b3d51906861018cc6d0e91d6d50977bfe4
Benjamin Midtvedt <benmid@student.chalmers.se>
2021-06-29 13:10:23 +0200

    Create menu item fro creating properties

--- src/ProjectPage/node-editor/ContextMenu/node-menu.js
+++ src/ProjectPage/node-editor/ContextMenu/node-menu.js
@@ -5,16 +5,48 @@ export default function (Menu) {
         constructor(editor, props, nodeItems) {
             super(editor, props);
 
+            this.addItem('Create properties', (args) => {
+                const {
+                    name,
+                    position: [x, y],
+                    inputs,
+                    ...params
+                } = args.node;
+                const property_node = editor?.components?.get('Property');
+                (editor.components.get(name)?.feature?.properties || []).forEach((prop, idx) => {
+                    const input = inputs.get(prop.name);
+                    console.log(input);
+                    property_node.createNode({}).then((p_node) => {
+                        p_node.meta = {};
+                        p_node.position[0] = args.node.position[0] - 300;
+                        p_node.position[1] = args.node.position[1] + 145 + 44 * idx;
+                        const control = p_node.controls.get('p_output');
+                        control.setValue.bind(control)(prop.value);
+
+                        editor?.addNode(p_node);
+
+                        // Connect
+                        const output = p_node.outputs.get('p_output');
+                        if (input && output) {
+                            editor?.connect(output, input);
+                        }
+                    });
+                });
+            });
             this.addItem('Delete', ({ node }) => editor.removeNode(node));
             this.addItem('Clone', async (args) => {
-                const { name, position: [x, y], ...params } = args.node;
+                const {
+                    name,
+                    position: [x, y],
+                    ...params
+                } = args.node;
                 const component = editor.components.get(name);
                 const node = await createNode(component, { ...params, x: x + 10, y: y + 10 });
 
                 editor.addNode(node);
             });
 
-            traverse(nodeItems, (name, func, path) => this.addItem(name, func, path))
+            traverse(nodeItems, (name, func, path) => this.addItem(name, func, path));
         }
-    }
+    };
 }
