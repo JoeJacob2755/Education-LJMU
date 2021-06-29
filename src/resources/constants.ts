082c34de188e9278f156f65d63c996a96265dfe0
Benjamin Midtvedt <benmid@student.chalmers.se>
2021-06-29 13:09:32 +0200

    rete naming constants

--- src/resources/constants.ts
+++ src/resources/constants.ts
@@ -7,6 +7,8 @@ export const CONTENT_DIR_NAME = 'Content';
 
 export const DEFAULT_FEATURE_PACKAGE_NAME = 'misc';
 
+export const FEATURE_RETE_INPUT_NAME = 'f_inpt';
+
 export const PYTHON_HEADER_DISCLAIMER = `
 # THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT!
 # To 
