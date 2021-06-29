c5537c3a6f59a3e222c536b3e2b6fc76c46f92f9
Benjamin Midtvedt <benmid@student.chalmers.se>
2021-06-29 13:09:42 +0200

    install additional rete plugins

--- src/ProjectPage/node-editor/editor.tsx
+++ src/ProjectPage/node-editor/editor.tsx
@@ -4,12 +4,14 @@ import Rete, { Node } from 'rete';
 import ReactRenderPlugin, { Node as NodeComponent } from 'rete-react-render-plugin';
 import ConnectionPlugin from 'rete-connection-plugin';
 // @ts-ignore
+import HistoryPlugin from 'rete-history-plugin';
+// @ts-ignore
 import ContextMenuPlugin, { ReactMenu } from './ContextMenu/index.js';
 // @ts-ignore
 import AreaPlugin from 'rete-area-plugin';
 import { Data, NodeData, WorkerInputs, WorkerOutputs } from 'rete/types/core/data';
 import PythonApi from '../../resources/PythonApi';
-import { ViewModuleSharp } from '@material-ui/icons';
+import { ReportRounded, ViewModuleSharp } from '@material-ui/icons';
 
 import { stringSocket, featureSocket, imageSocket } from './Sockets';
 import { Feature } from '../../resources/dtserver/grpc/Feature.js';
@@ -21,13 +23,15 @@ import { SOURCE_DIR_NAME } from '../../resources/constants';
 const FeatureControlComponent = ({
     value,
     onChange,
-    defaultValue,
-}: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) => (
+}: {
+    value: () => string | undefined;
+    onChange: ((event: React.ChangeEvent<HTMLInputElement>) => void) | undefined;
+}) => (
     <input
         className="feature-control-input"
         type="text"
-        defaultValue={defaultValue}
-        value={value}
+        // defaultValue={defaultValue}
+        value={typeof value == 'string' ? value : value()}
         ref={(ref) => {
             ref && ref.addEventListener('pointerdown', (e) => e.stopPropagation());
         }}
@@ -40,16 +44,20 @@ class FeatureControl extends Rete.Control {
     component = FeatureControlComponent;
     props: {
         readonly: boolean;
-        value: string | undefined;
+        value: string | (() => string | undefined);
         defaultValue?: string;
         onChange: ((event: React.ChangeEvent<HTMLInputElement>) => void) | undefined;
     };
 
     setValue(val: string) {
-        this.props.value = val;
         this.putData(this.key, val);
+        this.props.value = val;
         // @ts-ignore
-        this.update();
+        try {
+            this.update();
+        } catch (error) {
+            console.log('no update');
+        }
     }
 
     constructor(emitter: any, key: string, _: Node, readonly = false) {
@@ -59,7 +67,7 @@ class FeatureControl extends Rete.Control {
 
         this.props = {
             readonly,
-            value: undefined,
+            value: () => this.getData(this.key),
             onChange: (v) => {
                 this.setValue(v.target.value);
                 this.emitter.trigger('process');
@@ -88,39 +96,39 @@ export class FeatureComponent extends Rete.Component {
         node.addInput(inpt).addOutput(outp).addOutput(feat);
 
         const property_node = this.editor?.components?.get('Property');
-        console.log(this.feature.properties);
-        this.feature?.properties?.forEach((prop, idx) => {
+        this.feature?.properties?.forEach((prop) => {
             if (!prop) return;
             let { name, value } = prop;
             console.log(name, value, prop);
             const input = new Rete.Input(name, name, stringSocket);
             node.addInput(input);
-            if (property_node) {
-                property_node.createNode({}).then((p_node: Node) => {
-                    let iters = 0;
 
-                    p_node.controls.get('p_output').props.defaultValue = value;
-                    // This is a bad hack. It's done since the position of the node is not known at this point.
-                    const c = setInterval(() => {
-                        iters++;
-                        if ((node.position[0] !== 0 && node.position[1] !== 0) || iters > 5) {
-                            p_node.meta = {};
-                            p_node.position[0] = node.position[0] - 300;
-                            p_node.position[1] = node.position[1] + 145 + 44 * idx;
-                            this.editor?.addNode(p_node);
+            // if (property_node) {
+            //     property_node.createNode({}).then((p_node: Node) => {
+            //         let iters = 0;
 
-                            // Connect
-                            const output = p_node.outputs.get('p_output');
-                            if (input && output) {
-                                this.editor?.connect(output, input);
-                            }
+            //         p_node.controls.get('p_output').props.value = value;
+            //         // This is a bad hack. It's done since the position of the node is not known at this point.
+            //         const c = setInterval(() => {
+            //             iters++;
+            //             if ((node.position[0] !== 0 && node.position[1] !== 0) || iters > 5) {
+            //                 p_node.meta = {};
+            //                 p_node.position[0] = node.position[0] - 300;
+            //                 p_node.position[1] = node.position[1] + 145 + 44 * idx;
+            //                 this.editor?.addNode(p_node);
 
-                            // console.log(p_node.data, this.feature.properties[name].default);
-                            clearInterval(c);
-                        }
-                    }, 100);
-                });
-            }
+            //                 // Connect
+            //                 const output = p_node.outputs.get('p_output');
+            //                 if (input && output) {
+            //                     this.editor?.connect(output, input);
+            //                 }
+
+            //                 // console.log(p_node.data, this.feature.properties[name].default);
+            //                 clearInterval(c);
+            //             }
+            //         }, 100);
+            //     });
+            // }
         });
     }
 
@@ -160,6 +168,7 @@ export async function createEditor(container: HTMLElement, dataPath: string) {
 
     var editor = new Rete.NodeEditor('demo@0.1.0', container);
     editor.use(ConnectionPlugin);
+    editor.use(HistoryPlugin, { keyboard: false });
     editor.use(ReactRenderPlugin);
     editor.use(ContextMenuPlugin, {
         Menu: ReactMenu,
@@ -168,6 +177,7 @@ export async function createEditor(container: HTMLElement, dataPath: string) {
             return [component.package];
         },
     });
+    editor.use(AreaPlugin, { snap: 10, scaleExtent: { min: 0.1, max: 4 } });
 
     var engine = new Rete.Engine('demo@0.1.0');
 
@@ -180,6 +190,36 @@ export async function createEditor(container: HTMLElement, dataPath: string) {
     const p_c = new PropertyComponent();
     editor.register(p_c);
     engine.register(p_c);
+
+    let boundTranslate = false;
+    container.addEventListener('mousedown', (e) => {
+        if (!e.ctrlKey) return;
+        boundTranslate = true;
+    });
+
+    document.addEventListener('mouseup', (e) => {
+        boundTranslate = false;
+    });
+
+    editor.on('keydown', (e) => {
+        console.log(e.key);
+        if (e.key === 'Delete') {
+            editor.selected.each((n) => editor.removeNode(n));
+        } else if (e.ctrlKey && !e.shiftKey && e.code === 'KeyA') {
+            editor.nodes.forEach((n) => editor.selectNode(n, true));
+            e.stopPropagation();
+            e.preventDefault();
+        } else if (e.ctrlKey && e.code === 'KeyZ') {
+            console.log(e.shiftKey);
+            editor.trigger(e.shiftKey ? 'redo' : 'undo');
+        }
+    });
+    editor.on('translate', () => {
+        if (boundTranslate) {
+            return false;
+        }
+    });
+
     // @ts-ignore
     editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
         console.log('process');
